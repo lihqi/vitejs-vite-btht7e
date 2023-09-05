f4d5a4c0c06c0b6da361678008bc225b14ec5b62
lihqi <455711093@qq.com>
2023-09-05 18:44:19 +0800

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import ReactDom from 'react-dom';
+import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'katex/dist/katex.min.css'; // `rehype-katex` does not import the CSS for you
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ReactMarkdown
+      children={`The lift coefficient ($C_L$) is a dimensionless coefficient.`}
+      remarkPlugins={[remarkMath]}
+      rehypePlugins={[rehypeKatex]}
+    />
+  );
+};
 
-export default App
+export default App;
