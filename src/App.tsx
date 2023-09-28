e9f4838c709a310760782bb84e60ad33e273320b
lihuaqi <lihuaqi@sensetime.com>
2023-09-28 15:21:19 +0800

    chore: try leafer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import ReactMarkdown from 'react-markdown';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
-import 'katex/dist/katex.min.css'; // `rehype-katex` does not import the CSS for you
+import { Leafer, Text, Debug } from "leafer-ui";
+import { useEffect, useRef } from "react";
+import RectsCase from "./rectsDemo";
 
 const App = () => {
-  return (
-    <ReactMarkdown
-      children={`The lift coefficient ($C_L$) is a dimensionless coefficient.`}
-      remarkPlugins={[remarkMath]}
-      rehypePlugins={[rehypeKatex]}
-    />
-  );
+  const instance = useRef();
+  useEffect(() => {
+    if (!instance.current) {
+      const startTime = Date.now();
+      const app = new Leafer({ view: window });
+      instance.current = app;
+      Debug.enable = true;
+      Debug.filter = "RunTime";
+      // new RectsCase(app, 1); // 100万个
+      console.log(`创建100万个矩形用时：`, Date.now() - startTime, "毫秒");
+      const text = new Text({
+        fill: "rgb(50,205,121)",
+        text: "Welcome to LeaferJS",
+        stroke: {
+          type: "linear",
+          stops: [
+            { offset: 0, color: "#FF4B4B" },
+            { offset: 1, color: "#FEB027" },
+          ],
+        },
+      });
+      app.add(text);
+    }
+  }, []);
+
+  return <div>8888</div>;
 };
 
 export default App;
